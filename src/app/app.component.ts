1ea963f6755b3e8fca25815618cbba179785b9ba
Mitchell Keenan <Mitchell Keenan>
2017-04-20 20:51:55 -0300

    Fix title, remove buttons until they function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { UserService } from './user.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Chirps!';
+  title = 'Chirp!';
 
   constructor(private userService: UserService) { }
 }
